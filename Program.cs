8c42e0d02acda381588aeeda0c1e7c284993757e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-20 21:55:43 -0300

    pronto

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+String NomeUsuario;
+
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("--- Nome Ninja ---");
+Console.ResetColor();
+
+Console.WriteLine();
+Console.Write("Digite seu nome: ");
+
+NomeUsuario = Console.ReadLine();
+
+NomeUsuario = NomeUsuario.Trim().ToLower()
+                                     .Replace("a", "KA")
+                                     .Replace("b", "ZU")
+                                     .Replace("c", "MI")
+                                     .Replace("d", "TE")
+                                     .Replace("e", "KU")
+                                     .Replace("f", "LU")
+                                     .Replace("g", "GI")
+                                     .Replace("h", "RI")
+                                     .Replace("i", "KI")
+                                     .Replace("j", "ZUS")
+                                     .Replace("k", "ME")
+                                     .Replace("l", "TA")
+                                     .Replace("m", "RIM")
+                                     .Replace("n", "TO")
+                                     .Replace("o", "MO")
+                                     .Replace("p", "NO")
+                                     .Replace("q", "KE")
+                                     .Replace("r", "SHI")
+                                     .Replace("s", "ARI")
+                                     .Replace("t", "CHI")
+                                     .Replace("u", "DO")
+                                     .Replace("v", "RU")
+                                     .Replace("w", "MEI")
+                                     .Replace("x", "NA")
+                                     .Replace("y", "FU")
+                                     .Replace("z", "ZI");
+
+Console.WriteLine($"Seu nome ninja é {NomeUsuario.Substring(0,1)}{NomeUsuario.ToLower().Substring(1)}");
+
